775f801ab496f5f876d33821a2e3344b92fe209e
gaoxinlei <xinleigao@github.com>
2018-10-05 11:49:19 +0800

    'hdfs文件夹删除和自动设置uri源码分析'

--- src/test/java/com/example/HdfsTest.java
+++ src/test/java/com/example/HdfsTest.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 
 public class HdfsTest {
 
@@ -40,4 +39,37 @@ public class HdfsTest {
         system.copyToLocalFile(false,src,dest,true);
         LOGGER.info("成功将文件:{}下载到:{}",src,dest);
     }
+
+    @Test
+    public void testMkDir ()throws Exception{
+        System.setProperty("HADOOP_USER_NAME", "hadoop");
+        Configuration configuration = new Configuration();
+        FileSystem system = FileSystem.get(configuration);
+        boolean result = system.mkdirs(new Path("hdfs://stack:9000/user/hadoop/test"));
+        if(result){
+            LOGGER.info("创建文件夹成功");
+        }else{
+            LOGGER.info("创建文件夹失败");
+        }
+    }
+
+    /**
+     * 源码解析：
+     * 创建path时不需要指定前缀。
+     * 在path.getFileSystem(conf)时传入一个空的配置，FileSystem会去从conf中get
+     * 这个常量FS_DEFAULT_NAME_KEY，即fs.defaultFS,conf会去从两个默认文件
+     * core-default.xml和core-site.xml中取值。先设置的为默认值file:///，后从我们的
+     * core-site.xml中取出了设置的hdfs://stack:9000/
+     * @throws Exception
+     */
+    @Test
+    public void testDelete() throws Exception{
+        System.setProperty("HADOOP_USER_NAME", "hadoop");
+        Path path = new Path("/user/hadoop/test");
+        FileSystem system = path.getFileSystem(new Configuration());
+        //第二个参数是递归，若要删除非空文件夹必须指定为true。
+        system.delete(path,true);
+        LOGGER.info("成功删除了文件夹");
+    }
+
 }
